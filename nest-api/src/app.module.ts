b92360147c890a629dd3aea3c62f0bd68ae5ecb3
Roberto <r.filho1@hotmail.com>
2021-10-28 21:38:35 -0300

    feat(user): add schema

--- nest-api/src/app.module.ts
+++ nest-api/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    //MongooseModule.forRoot("mongodb://user:senha@host:port/nome_da_base")
+    MongooseModule.forRoot("mongodb://localhost:27017/nest-api-db"),
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
